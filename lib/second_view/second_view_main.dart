1a93fe8571e7c96662e4aa27c7321ddd51c560b4
Klaudiusz Kalinowski <klaudiusz.kalinowski01@wp.pl>
2023-04-07 23:48:27 +0200

    Animation on second screen

--- lib/second_view/second_view_main.dart
+++ lib/second_view/second_view_main.dart
@@ -1,9 +1,8 @@
 import 'package:currency_app/main_view/top_bar_button_circles.dart';
 import 'package:flutter/material.dart';
 import 'package:currency_app/main_view/front_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import '../fetch_JSON/get_data.dart';
-import '../fetch_JSON/model_class.dart';
 
 class SecondView extends StatefulWidget {
   const SecondView({super.key});
@@ -12,41 +11,69 @@ class SecondView extends StatefulWidget {
   State<SecondView> createState() => _SecondViewState();
 }
 
-class _SecondViewState extends State<SecondView> {
+class _SecondViewState extends State<SecondView>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 1500),
+  )..forward();
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInOutBack,
+  );
+
   @override
-  void initState() {
-    super.initState();
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 16, left: 16),
-                  child: Builder(builder: (context) {
-                    return const MenuIcon();
-                  }),
+        child: FadeTransition(
+          opacity: _animation,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 16.h, left: 16.w),
+                    child: Builder(builder: (context) {
+                      return const MenuIcon();
+                    }),
+                  ),
+                  SizedBox(
+                      width: 200.w, height: 125.h, child: const CirclesCorner())
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 56.w),
+                child: const MainText(), //TODO: Zmienić na bez animacji
+              ),
+              // TextButton(
+              //     onPressed: () {
+              //       Get.back();
+              //     },
+              //     child: const Text("Back")),
+              SizedBox(
+                height: 48.h,
+              ),
+              Expanded(
+                // * Zrobić z tego klase
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadiusDirectional.only(
+                          topStart: Radius.circular(40.r),
+                          topEnd: Radius.circular(40.r))),
                 ),
-                const SizedBox(width: 200, height: 125, child: CirclesCorner())
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 56),
-              child: AnimatedMainText(), //TODO: Zmienić na bez animacji
-            ),
-            TextButton(
-                onPressed: () {
-                  Get.back();
-                },
-                child: const Text("Back"))
-          ],
+              )
+            ],
+          ),
         ),
       ),
       drawer: const MyDrawerClass(), // TODO: Zrobić ten drawer do końca
